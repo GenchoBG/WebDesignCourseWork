6ec010126a371e4ed9f6a8279aa5eff4ad3499cd
GenchoBG <genchobg11@gmail.com>
2019-04-28 19:40:29 +0300

    Register button is now disabled if passwords do not match

--- scripts/register.js
+++ scripts/register.js
@@ -2,7 +2,9 @@ function validatePasswordsMatch() {
     if ($('#inputPassword').val() === $('#inputConfirmPassword').val()) {
         $('#inputPassword').css('color', 'green');
         $('#inputConfirmPassword').css('color', 'green');
+        $("#register-btn").removeAttr("disabled");
     } else {
+        $("#register-btn").attr("disabled", "disabled");
         $('#inputPassword').css('color', 'red');
         $('#inputConfirmPassword').css('color', 'red');        
     }
